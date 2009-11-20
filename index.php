39112eae3f4446c959c7ecf8a82e67b88fa6b5b0
Javier Aravena Claramunt <javier@aravenas.com>
2009-11-20 17:30:38 +0000

    Visual improvements * Now machines are displayed on a table.

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
    /*
     * Copyright 2009 Javier Aravena Claramunt.
     *
@@ -19,30 +18,66 @@
     * along with XenList.  If not, see <http://www.gnu.org/licenses/>.
     */
 
-   $servers = array('ethan', 'locke');
-   $rawdata  = array();
-   $domain = array();
-   foreach($servers as $server) {
-      $rawdata[$server] = "";
-      $file = fopen("http://$server:8080/list.json", "r");
-      while(!feof($file))
-      $rawdata[$server] = $rawdata[$server] . fgets($file, 4096);
-      fclose($file);
-      $domain[$server] = json_decode($rawdata[$server]);
-   }
+$servers = array('ethan', 'locke', 'ben', 'faraday', 'desmond');
+
+function server_picture($server, $class) {
+    if(file_exists("img/$server.jpg")) $extension = 'jpg';
+    if(file_exists("img/$server.png")) $extension = 'png';
+    if(file_exists("img/$server.gif")) $extension = 'gif';
+    if(isset($extension))
+        print('<img src="img/' . $server . '.' .$extension . '" class="' . $class . '"/>');
+}
+
+$rawdata  = array();
+$domain = array();
+foreach($servers as $server) {
+	$rawdata[$server] = "";
+	$file = fopen("http://$server:8080/list.json", "r");
+	while(!feof($file))
+		$rawdata[$server] = $rawdata[$server] . fgets($file, 4096);
+	fclose($file);
+	$domain[$server] = json_decode($rawdata[$server]);
+}
 
 
 ?>
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
-   <head>
-      <title>Xen List</title>
-   </head>
-   <body>
-      <pre>
-         <?php print_r($domain); ?>
-      </pre>
-   </body>
+  <head>
+    <title>Xen List</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+  <body>
+    <h1>Xen List</h1>
+    <table id="list">
+      <tr>
+        <?php foreach($domain as $s=>$server): ?>
+          <th>
+            <?php 
+              server_picture($s, 'server-avatar');
+              print($s); 
+            ?>
+          </th>
+        <?php endforeach; ?>
+      </tr>
+      <tr>
+        <?php foreach($domain as $server): ?>
+           <td>
+             <ul>
+               <?php foreach($server as $s): ?>
+               <li>
+                 <?php 
+                   server_picture($s, 'vserver-avatar');
+                   print($s); 
+                 ?>
+               </li>
+               <?php endforeach; ?>
+             </ul>
+           </td>
+        <?php endforeach; ?>
+      </tr>
+    </table>
+  </body>
 </html>
